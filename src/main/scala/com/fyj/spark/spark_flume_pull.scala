ea833db90f2bee79535358fb1ea3263c15131007
2NaCl <qishli4609@qq.com>
2019-08-16 19:05:54 +0800

    20190816

---
+++ src/main/scala/com/fyj/spark/spark_flume_pull.scala
@@ -0,0 +1,27 @@
+package com.fyj.spark
+
+import org.apache.spark.SparkConf
+import org.apache.spark.streaming.flume.FlumeUtils
+import org.apache.spark.streaming.{Seconds, StreamingContext}
+
+object spark_flume_pull {
+
+  def main(args: Array[String]): Unit = {
+
+//    if(args.length!=2) {
+//      System.exit(1)
+//    }
+//
+//    var Array(hostname,port) = args
+
+    var sc = new SparkConf().setAppName("spark-flume").setMaster("local[*]")
+    var ssc = new StreamingContext(sc, Seconds(5))
+
+    val flumeStream = FlumeUtils.createPollingStream(ssc,"0.0.0.0",41414)
+    flumeStream.map(x=> new String(x.event.getBody.array()).trim)
+      .flatMap(_.split(" ")).map((_,1)).reduceByKey(_+_).print()
+    ssc.start()
+    ssc.awaitTermination()
+  }
+
+}
